e13aa8ef804e0ec37592b2fbb0d426f6c9b5bad3
Felipe Lima <pegasusyt1@gmail.com>
2024-06-03 11:55:54 -0300

    updated env name

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -7,7 +7,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
@@ -56,7 +56,7 @@ export async function POST(req: Request) {
 
   const eventType = evt.type;
 
-  console.log({eventType});
+  console.log({ eventType });
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
