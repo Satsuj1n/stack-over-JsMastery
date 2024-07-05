7723fbcdbe0c74c27c9216d84b3c16c2d43fff9a
Felipe Lima <pegasusyt1@gmail.com>
2024-07-05 10:38:19 -0300

    Implementação dos upvotes e downvotes e favoritos

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -17,7 +17,7 @@ const Page = async ({ params }) => {
   const { userId: clerkId } = auth();
 
   let mongoUser;
-  if(clerkId) {
+  if (clerkId) {
     mongoUser = await getUserById({ userId: clerkId });
   }
 
@@ -41,7 +41,16 @@ const Page = async ({ params }) => {
             </p>
           </Link>
           <div className="flex justify-end">
-            <Votes/>
+            <Votes
+              type="Question"
+              itemId={JSON.stringify(result._id)}
+              userId={JSON.stringify(mongoUser._id)}
+              upvotes={result.upvotes.length}
+              hasupVoted={result.upvotes.includes(mongoUser._id)}
+              downvotes={result.downvotes.length}
+              hasdownVoted={result.downvotes.includes(mongoUser._id)}
+              hasSaved={mongoUser?.saved.includes(result._id)}
+            />
           </div>
         </div>
         <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
@@ -86,10 +95,10 @@ const Page = async ({ params }) => {
       </div>
       <AllAnswers
         questionId={result._id}
-        userId={JSON.stringify(mongoUser._id)}
+        userId={mongoUser._id}
         totalAnswers={result.answers.length}
       />
-      <Answer 
+      <Answer
         question={result.content}
         questionId={JSON.stringify(result._id)}
         authorId={JSON.stringify(mongoUser._id)}
--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,9 +1,156 @@
-import React from 'react'
+"use client";
+import { downvoteAnswer, upvoteAnswer } from "@/lib/actions/answer.action";
+import {
+  downvoteQuestion,
+  upvoteQuestion,
+} from "@/lib/actions/question.action";
+import { toggleSaveQuestion } from "@/lib/actions/user.action";
+import { formatAndDivideNumber } from "@/lib/utils";
+import Image from "next/image";
+import { usePathname,useRouter  } from "next/navigation";
+import React from "react";
 
-const Votes = () => {
-  return (
-    <div>Votes</div>
-  )
+interface Props {
+  type: string;
+  itemId: string;
+  userId: string;
+  upvotes: number;
+  hasupVoted: boolean;
+  downvotes: number;
+  hasdownVoted: boolean;
+  hasSaved?: boolean;
 }
+const Votes = ({
+  type,
+  itemId,
+  userId,
+  upvotes,
+  hasupVoted,
+  downvotes,
+  hasdownVoted,
+  hasSaved,
+}: Props) => {
+  const pathname = usePathname();
+ // const router = useRouter();
+  const handleSave = async () => {
+    await toggleSaveQuestion({
+      questionId: JSON.parse(itemId),
+      userId: JSON.parse(userId),
+      path: pathname,
+    });
+  };
+  const handleVote = async (action: string) => {
+    if (!userId) {
+      // redirect to login
+      return;
+    }
+    if (action === "upvote") {
+      if (type === "Question") {
+        await upvoteQuestion({
+          questionId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasdownVoted,
+          hasupVoted,
+          path: pathname,
+        });
+      } else if (type === "Answer") {
+        await upvoteAnswer({
+          answerId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasdownVoted,
+          hasupVoted,
+          path: pathname,
+        });
+      }
+      // todo: show a toast
+      return;
+    }
+    if (action === "downvote") {
+      if (type === "Question") {
+        await downvoteQuestion({
+          questionId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasdownVoted,
+          hasupVoted,
+          path: pathname,
+        });
+      } else if (type === "Answer") {
+        await downvoteAnswer({
+          answerId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasdownVoted,
+          hasupVoted,
+          path: pathname,
+        });
+      }
+      // todo: show a toast
+    }
+  };
+  return (
+    <div className="flex gap-5">
+      <div className="flex-center gap-2.5">
+        <div className="flex-center gap-1.5">
+          <Image
+            src={
+              hasupVoted
+                ? "/assets/icons/upvoted.svg"
+                : "/assets/icons/upvote.svg"
+            }
+            height={18}
+            width={18}
+            alt="upvote icon"
+            className="cursor-pointer"
+            onClick={() => {
+              handleVote("upvote");
+            }}
+          />
 
-export default Votes
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatAndDivideNumber(upvotes ?? 0)}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex-center gap-1.5">
+          <Image
+            src={
+              hasdownVoted
+                ? "/assets/icons/downvoted.svg"
+                : "/assets/icons/downvote.svg"
+            }
+            height={18}
+            width={18}
+            alt="downvote icon"
+            className="cursor-pointer"
+            onClick={() => {
+              handleVote("downvote");
+            }}
+          />
+
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatAndDivideNumber(downvotes ?? 0)}
+            </p>
+          </div>
+        </div>
+      </div>
+      {type === "Question" && (
+        <Image
+          src={
+            hasSaved
+              ? "/assets/icons/star-filled.svg"
+              : "/assets/icons/star-red.svg"
+          }
+          height={18}
+          width={18}
+          alt="star icon"
+          className="cursor-pointer"
+          onClick={handleSave}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Votes;
